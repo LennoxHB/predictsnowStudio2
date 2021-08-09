85b6d6ab91a26dde173c36be23b09f0773d21f0d
LennoxHB <lennoxhwaru@hotmail.com>
2021-08-10 11:11:55 +1200

    Add main menu

--- Predictsnow/Predictsnow/Program.cs
+++ Predictsnow/Predictsnow/Program.cs
@@ -4,9 +4,28 @@ namespace Predictsnow
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string temp;
+            int start = 1;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Predict Snow Menu\n\n1. Enter Data.\n2. Exit.");
+                temp = Console.ReadLine();
+                switch (temp)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        start = 0;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid input\n\nPress ENTER to continue");
+                        break;
+                }
+            } while (start == 1);
         }
     }
 }
