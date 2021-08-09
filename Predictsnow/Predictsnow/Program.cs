6cba4f77453875b3c3f0c548db4ea8b67c0b8774
JadenCooper <83618082+JadenCooper@users.noreply.github.com>
2021-08-10 11:31:34 +1200

    Update Program.cs

--- Predictsnow/Predictsnow/Program.cs
+++ Predictsnow/Predictsnow/Program.cs
@@ -4,6 +4,35 @@ namespace Predictsnow
 {
     class Program
     {
+                public static int[] Stats()
+        {
+            int[] Snow_Stats = new int[4];
+            bool Correct = false;
+            int temp;
+            do
+            {
+                Console.WriteLine("Please Enter The Air Tempeture");
+                Snow_Stats[0] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please Enter The Ground Tempeture");
+                Snow_Stats[1] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please Enter The Wind Direction 1 for North 2 For South 3 For East 4 For West");
+                Snow_Stats[2] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please Enter The Humidity");
+                Snow_Stats[3] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Would You Like To Reenter Your Figures Or Are They Correct? 1 To Go Back 2 For Correct");
+                temp = Convert.ToInt32(Console.ReadLine());
+                if (temp == 1)
+                {
+                    Correct = false;
+                }
+                else
+                {
+                    Correct = true;
+                }
+            } while (Correct == true);
+            return Snow_Stats[];
+
+        }
         static void Predic()
         {
             Console.WriteLine("temp check thing");
